218c2e04b6adb812af37da8950778e9f71c711cb
Darrell Aucutt <daucutt@gmail.com>
2018-06-27 10:28:35 -0700

    Hackily handling more response codes.

--- maucutt.js
+++ maucutt.js
@@ -7,6 +7,7 @@ http.createServer(function(req, response) {
 
      var uri = url.parse(req.url).pathname; 
      var path =  uri.substring(1,uri.length);
+     process.stdout.write("path: " +  path +  "\n");
      var filename = "responses" + uri + ".json"; 
 
      if( path == "304") {
@@ -17,6 +18,68 @@ http.createServer(function(req, response) {
          return;
 
      }
+     if( path == "403") {
+         process.stdout.write("403.\n");
+         var timestamp = "{\"Date\":" +  new Date().getTime(); +  "\"}";
+         response.writeHead(403, timestamp);
+         response.end();
+         return;
+
+     }
+     if (path == "400" ) {
+         process.stdout.write("400.\n");
+         var timestamp = "{\"Date\":" +  new Date().getTime(); +  "\"}";
+         response.writeHead(400, timestamp);
+         response.end();
+         return;
+
+ 
+     }
+     if (path == "404" ) {
+         process.stdout.write("404\n");
+         var timestamp = "{\"Date\":" +  new Date().getTime(); +  "\"}";
+         response.writeHead(404, timestamp);
+         response.end();
+         return;
+
+    }
+  if (path == "405" ) {
+         process.stdout.write("405.\n");
+         var timestamp = "{\"Date\":" +  new Date().getTime(); +  "\"}";
+         response.writeHead(405, timestamp);
+         response.end();
+         return;
+
+    }
+
+  if (path == "429" ) {
+         process.stdout.write("429.\n");
+         var timestamp = "{\"Date\":" +  new Date().getTime(); +  "\"}";
+         response.writeHead(429, timestamp);
+         response.end();
+         return;
+
+    }
+
+  if (path == "500" ) {
+         process.stdout.write("500.\n");
+         var timestamp = "{\"Date\":" +  new Date().getTime(); +  "\"}";
+         response.writeHead(500, timestamp);
+         response.end();
+         return;
+
+    }
+
+	if (path == "205" ) {
+         process.stdout.write("205.\n");
+         var timestamp = "{\"Date\":" +  new Date().getTime(); +  "\"}";
+         response.writeHead(205, {"Content-Type": "application/json; charset=utf-8"}, {"Content-Length": "4"});
+         response.write("{\n}\n");
+         response.end();
+         return;
+
+    }
+ 
      process.stdout.write("request:  " +  uri +  " maps to "  + filename +  "\n");
      fs.exists(filename, function(exists) {
         if(!exists) {
